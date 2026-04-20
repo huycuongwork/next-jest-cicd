f35b38fb5543592fef59c5526a3aff51314b2d7b
huycuong <cuongxuyentu99@gmail.com>
2026-04-20 13:59:37 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -19,7 +19,7 @@ export async function generateMetadata({ params }: Params) {
 export default function Page({ params }: Params) {
   return (
     <>
-      <h1>Slug123: {params.slug}</h1>
+      <h1>Slug: {params.slug}</h1>
       <p>hole tex</p>
       <p>welcome to HCM</p>
     </>
