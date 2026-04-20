88b29eb147bb856f0f06821fa0d4714382447b58
huycuong <cuongxuyentu99@gmail.com>
2026-04-20 12:56:36 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,11 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <h1>Slug: {params.slug}</h1>;
+  return (
+    <>
+      <h1>Slug: {params.slug}</h1>
+      <p>hole tex</p>
+      <p>welcome to HCM</p>
+    </>
+  );
 }
